0d02c0e4783bcbc0e54b3971dea808c4a616c1de
Sazid <sazidozon@gmail.com>
2019-11-24 13:13:35 +0600

    Configure database
    Signed-off-by: Sazid <sazidozon@gmail.com>

--- ems2/settings.py
+++ ems2/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'ems2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ems2',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
