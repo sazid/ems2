2034fe4db3143c0318249c89a5689674a63ba4db
sazid <sazidozon@gmail.com>
2019-12-19 02:24:15 +0600

    Fix new user app related issues

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    list_display = ('username', 'first_name', 'last_name',)
+    fieldsets = (
+        ('Login', {'fields': (
+            'username',
+            'password'
+        )}),
+        ('Personal Information', {'fields': (
+            'first_name',
+            'last_name',
+            'email',
+        )}),
+        ('University', {'fields': (
+            'role',
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+        )}),
+    )
+
+
+# Register User model
+admin.site.register(User, UserAdmin)
+
+# Unregister Django's builtin user group model
+admin.site.unregister(Group)
